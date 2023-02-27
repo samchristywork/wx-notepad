a2deef5ba0815e5ab3e7e27ce8f26621c0d4a54a
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-02-26 20:56:58 -0600

    Initial code release: Added generated code

---
+++ codegen/noname.cpp
@@ -0,0 +1,32 @@
+///////////////////////////////////////////////////////////////////////////
+// C++ code generated with wxFormBuilder (version 3.10.1-0-g8feb16b3)
+// http://www.wxformbuilder.org/
+//
+// PLEASE DO *NOT* EDIT THIS FILE!
+///////////////////////////////////////////////////////////////////////////
+
+#include "noname.h"
+
+///////////////////////////////////////////////////////////////////////////
+
+MainFrame::MainFrame( wxWindow* parent, wxWindowID id, const wxString& title, const wxPoint& pos, const wxSize& size, long style ) : wxFrame( parent, id, title, pos, size, style )
+{
+	this->SetSizeHints( wxDefaultSize, wxDefaultSize );
+
+	m_statusBar = this->CreateStatusBar( 1, wxSTB_SIZEGRIP, wxID_ANY );
+	wxBoxSizer* bSizer;
+	bSizer = new wxBoxSizer( wxVERTICAL );
+
+	m_textCtrl = new wxTextCtrl( this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize, wxTE_MULTILINE );
+	bSizer->Add( m_textCtrl, 1, wxALL|wxEXPAND, 5 );
+
+
+	this->SetSizer( bSizer );
+	this->Layout();
+
+	this->Centre( wxBOTH );
+}
+
+MainFrame::~MainFrame()
+{
+}
