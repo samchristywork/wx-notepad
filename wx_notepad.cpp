7303b5c6154f8c87696f973e40aa61679990771c
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-02-27 21:55:44 -0600

    Feature: Added initial menu bar ite

--- wx_notepad.cpp
+++ wx_notepad.cpp
@@ -10,11 +10,32 @@ public:
   virtual bool OnInit();
 };
 
+void add_menu_item(wxMenu *menu, wxString text, int id) {
+  wxMenuItem *menuItem =
+      new wxMenuItem(menu, id, text, wxEmptyString, wxITEM_NORMAL);
+
+  menu->Append(menuItem);
+}
+
 class Frame : public MainFrame {
 public:
-  Frame(wxWindow *parent) : MainFrame(parent) {}
-  void hello_world(wxCommandEvent &event) {
-    puts("hi");
+  Frame(wxWindow *parent) : MainFrame(parent) {
+
+    wxMenuBar *menuBar = new wxMenuBar(0);
+
+    wxMenu *menuFile = new wxMenu();
+
+    add_menu_item(menuFile, wxString(wxT("Exit")), ID_Exit);
+
+    menuBar->Append(menuFile, wxT("File"));
+
+    this->SetMenuBar(menuBar);
+
+    Bind(wxEVT_MENU, &Frame::Exit, this, ID_Exit);
+  }
+
+  void Exit(wxCommandEvent &event) { this->Destroy(); }
+
   }
 };
 
