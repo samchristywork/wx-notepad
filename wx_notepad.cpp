e25a96c6fa45389a35407a595821920ff7791a60
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-03-09 19:19:26 -0600

    Refactor: Removed remaining impls from class def

--- wx_notepad.cpp
+++ wx_notepad.cpp
@@ -64,14 +64,22 @@ public:
   FindStrWindow(wxWindow *parent, wxTextCtrl *textCtrl);
   void FindNext();
 
-  virtual void FindNextClickCallback(wxCommandEvent &event) {
-    this->FindNext();
-  }
-
-  virtual void CancelClickCallback(wxCommandEvent &event) { this->Destroy(); }
-
-  virtual void FindEnterCallback(wxCommandEvent &event) { this->FindNext(); }
+  virtual void FindNextClickCallback(wxCommandEvent &event);
+  virtual void CancelClickCallback(wxCommandEvent &event);
+  virtual void FindEnterCallback(wxCommandEvent &event);
 };
+
+void FindStrWindow::CancelClickCallback(wxCommandEvent &event){
+  this->Destroy();
+}
+
+void FindStrWindow::FindEnterCallback(wxCommandEvent &event){
+  this->FindNext();
+}
+
+void FindStrWindow::FindNextClickCallback(wxCommandEvent &event){
+  this->FindNext();
+}
 
 FindStrWindow::FindStrWindow(wxWindow *parent, wxTextCtrl *textCtrl)
     : FindFrame(parent) {
